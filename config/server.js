67ed14b296bdd86b037a4d476ae362f7b2109d27
Raimon López <raimon@bokokode.com>
2024-06-29 21:44:43 +0200

    fixed sec vulnerability

--- config/server.js
+++ config/server.js
@@ -2,9 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS", [
-      "Oq6rBkMw9WgsaxNF8UZXztGVEv+Ibh9T5fx60S4cK3g=",
-    ]),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
