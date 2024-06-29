4e249117360ca21fdaaf8b60e14e934322ef3e99
Raimon López <raimon@bokokode.com>
2024-06-29 22:46:59 +0200

    tried fixing cors ERROR

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,26 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: false,
+    },
+  },
+  "strapi::cors", // Enable CORS
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3001", ""], // Add your frontend URLs here
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
